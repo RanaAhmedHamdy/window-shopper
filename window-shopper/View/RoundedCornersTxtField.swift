5f988a287f0be5718713414a32c87cfa082b7c3e
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-22 15:25:48 +0200

    create design

---
+++ window-shopper/View/RoundedCornersTxtField.swift
@@ -0,0 +1,37 @@
+//
+//  RoundedCornersTxtField.swift
+//  window-shopper
+//
+//  Created by Hazem Mohamed Magdy on 11/22/17.
+//  Copyright © 2017 Rana. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedCornersTxtField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2859589041)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+
+    }
+}
