ca433a1b4e12e49bd1d63a37ee26697b7724e889
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-23 10:16:41 +0200

    add currency label

--- window-shopper/View/RoundedCornersTxtField.swift
+++ window-shopper/View/RoundedCornersTxtField.swift
@@ -11,6 +11,24 @@ import UIKit
 @IBDesignable
 class RoundedCornersTxtField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: frame.size.height / 2 - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0.7407962329)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLabel.text = formatter.currencySymbol
+        
+        addSubview(currencyLabel)
+        
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -22,6 +40,8 @@ class RoundedCornersTxtField: UITextField {
     }
     
     func customizeView() {
+        clipsToBounds = true
+        
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2859589041)
         layer.cornerRadius = 5.0
         textAlignment = .center
